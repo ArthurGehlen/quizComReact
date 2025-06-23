d89368a7fb9fbbde8bc69a2228f54597fe8a633e
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-06-22 23:05:57 -0300

    Começando a página inicial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Welcome from "./components/Welcome"
+
+import './App.css'
+
 function App() {
   return (
     <>
-      <h1>Quiz App</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </>
   )
 }
